80cc1250cca7723bb57228432b38e356fe0c0087
Carter Wright <skycreed04@gmail.com>
2024-06-22 23:24:05 -0400

    added display functionality

--- packages/gpu/video_main_memory.py
+++ packages/gpu/video_main_memory.py
@@ -1,11 +1,22 @@
 #imports
-from ..system_memory.memory import Memory
 
 #class
-class VRAM(Memory):
+class VRAM:
     def __init__(self):
 
-        self.frame_buffer = []
+        self.frame_buffer = [0 for i in range(0, 64)]
+
+        self.test_buffer = [['H   H  III'],
+                            ['H   H   I'],
+                            ['HHHHH   I'],
+                            ['H   H   I'],
+                            ['H   H  III']]
 
     def display_frame_buffer(self):
+        #change test buffer if created frame buffer functionality
+        for line in self.test_buffer:
+            for character in line:
+                print(character)
+
+    def write(self, data):
         pass
